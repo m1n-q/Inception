75c9e514f532205f3fcac8b285f757b2d189d261
m1n-q <m1n_q@naver.com>
2022-04-13 18:25:29 +0900

    issue: cannot connect to mariadb:3306

--- srcs/requirements/mariadb/docker-entrypoint.sh
+++ srcs/requirements/mariadb/docker-entrypoint.sh
@@ -2,13 +2,20 @@
 
 #TODO: if env does not exists
 
+# Do a temporary startup of the MariaDB server, for init purposes
+# mysql --daemonize
+if service mysql start;  then
+sleep 5 
 #init wp-database with env
 mysql <<- EOSQL
 	CREATE DATABASE $MYSQL_DATABASE;
 	CREATE USER '$MYSQL_USER'@'wordpress' IDENTIFIED BY '$MYSQL_PASSWORD';
 	GRANT ALL ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'wordpress' IDENTIFIED BY '$MYSQL_PASSWORD' WITH GRANT OPTION;
-	ALTER USER 'root'@'$MYSQL_ROOT_HOST' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD'
+	ALTER USER 'root'@'$MYSQL_ROOT_HOST' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
 	FLUSH PRIVILEGES;
 EOSQL
+fi
+# shutdown MariaDB server
+mysqladmin shutdown -uroot  -p$MYSQL_ROOT_PASSWORD
 
 exec mysqld
