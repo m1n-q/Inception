d831804a49418c853d988a6f414e643a158163b1
m1n-q <m1n_q@naver.com>
2022-04-19 12:03:07 +0900

    installation on local domain

--- srcs/requirements/wordpress/docker-entrypoint.sh
+++ srcs/requirements/wordpress/docker-entrypoint.sh
@@ -6,6 +6,7 @@ WP_ASSETS=/var/wp-assets/wordpress
 if [ ! -e "$WP_ASSETS" ]; then
 	mv /tmp/wordpress $WP_ASSETS;
 	mv /wp-config.php $WP_ASSETS/;
+	curl -d "weblog_title=MyWP&user_name=WORDMAN&admin_password=tooshort&admin_password2=tooshort&admin_email=ok@na.com" $WORDPRESS_URL:80/wp-admin/install.php?step=2
 fi
 
 #export nginx_ip=$(ping -c 1 nginx | grep PING | awk '{print $3 }' | tr -d '()')
