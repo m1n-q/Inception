176ccf5642da3fc0aa9eaaa062c14507c8dac642
m1n-q <m1nq.dev@gmail.com>
2022-07-21 21:09:16 +0900

    add init_vm script

---
+++ init_vm.sh
@@ -0,0 +1,12 @@
+sudo apt-get install curl
+sudo apt-get install -y git
+sudo apt-get install make
+sudo apt-get install -y vim
+
+
+# install docker
+#    ca-certificates \
+#    gnupg \
+#    lsb-release
+
+curl -fsSL https://get.docker.com -o ~/get_docker.sh && chmod u+o ~/get_docker.sh && ~/get_docker.sh;
